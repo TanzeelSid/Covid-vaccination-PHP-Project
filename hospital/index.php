ffc99898ccffbda3f3d02c113f92b23bb8857c70
Tanzeel Siddique <110907732+TanzeelSid@users.noreply.github.com>
2024-03-04 17:12:07 +0500

    updated mistakes

--- hospital/index.php
+++ hospital/index.php
@@ -7,7 +7,7 @@ session_start();
 
 <head>
     <meta charset="utf-8">
-    <title>Admin Dashboard</title>
+    <title>Hospital Dashboard</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
